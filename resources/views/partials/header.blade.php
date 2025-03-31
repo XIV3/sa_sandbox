b14539fc51f40af44800756a8ed4c8e70a6e0284
Adarsh Sojitra <adarshsojitra7@gmail.com>
2025-03-31 22:40:40 +0530

    Update header.blade.php
    Removed powered by serveravatar from header.

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -6,12 +6,6 @@
                     <span class="fw-bold" style="color: var(--primary-color); line-height: 1.1; font-size: 2.2rem;">Sandbox</span>
                 </div>
             </a>
-            <div class="d-flex align-items-center gap-3">
-                <a href="https://serveravatar.com" target="_blank" class="text-decoration-none d-flex align-items-center">
-                    <span class="d-none d-md-inline-block text-secondary">Powered by </span>
-                    <span class="fw-bold text-primary ms-1">ServerAvatar <i class="fas fa-external-link-alt fs-6 ms-1"></i></span>
-                </a>
-            </div>
         </div>
     </nav>
 </header>
