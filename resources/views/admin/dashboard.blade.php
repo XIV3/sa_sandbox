4c83dcf7c1a6787f67046b99efde1c5af6c0236f
XIV3 <58135608+XIV3@users.noreply.github.com>
2025-05-30 23:57:40 +0800

    Update dashboard.blade.php
    Removed conditional on permanent site option on modal form.

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -440,7 +440,7 @@
                             </div>
                             
                             <div>
-                                @if(App\Models\SystemSetting::where('meta_key', 'allow_permanent_sites')->meta_value == '1')
+                                // @if(App\Models\SystemSetting::where('meta_key', 'allow_permanent_sites')->meta_value == '1')
                                 <div class="flex items-start mb-3">
                                     <div class="flex h-5 items-center">
                                         <input id="permanent" name="permanent" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"> 
@@ -449,7 +449,7 @@
                                         <label for="permanent" class="font-medium text-gray-700">Create a permanent site that does not expire</label>
                                     </div>
                                 </div>
-                                @endif
+                                // @endif
                                 
                                 <div class="flex items-start mb-3">
                                     <div class="flex h-5 items-center">
