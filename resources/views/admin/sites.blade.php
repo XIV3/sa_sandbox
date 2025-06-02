ee55b97af78d8bf3b18b6264ac7b1b11863fb8da
XIV3 <58135608+XIV3@users.noreply.github.com>
2025-06-02 15:40:17 +0800

    Update sites.blade.php
    Change 'Action' header alignment to right.

--- resources/views/admin/sites.blade.php
+++ resources/views/admin/sites.blade.php
@@ -50,7 +50,7 @@
                                     <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">PHP Version</th>
                                     <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Email</th>
                                     <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Expires In</th>
-                                    <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Action</th>
+                                    <th scope="col" class="px-3 py-3.5 text-right text-sm font-semibold text-gray-900">Action</th>
                                     <!--
                                     <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-6">
                                         <span class="sr-only">Actions</span>
